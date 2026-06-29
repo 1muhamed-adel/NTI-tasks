d81f61f61e82e678ecb0a19581b6af60c63b9fa8
1muhamed-adel <muhamed.adel.fci@gmail.com>
2026-06-29 19:28:47 +0300

    design home.html

--- session-10/js/script.js
+++ session-10/js/script.js
@@ -1,7 +1,35 @@
-var p = prompt("enter ur name");
-// (p)? console.log(`welcome ${p}`) : console.log("invaled");
+var looping = (startNum, endNum, breakNum, contNum)=> {
+    if ((startNum  && endNum && breakNum  && contNum )!=undefined) {
+        for(var i = startNum; i<= endNum; i++){
+            if (i == breakNum) {
+               break 
+            }
+            if (i == contNum) {
+                continue
+            }
+            console.log(i)
+        }
+    }
+    else
+        alert("pls enter numbers")
+}
 
-var num ="mohamed"
-console.log(`${p} ${num}`)
-
-// console.log(num **=2)
+// looping( )
+// shift remove first ele
+// unshift add in first 
+// splice 
+//map !!!(search)
+var result = ()=> {
+    var course= ["html" ,"css"]
+    var check = prompt("enter course")
+    if (course.includes(check)) {
+        console.log("found")
+    }
+    else{
+        course.push(check)
+    }
+  
+    console.log(course)
+    
+}
+result()
