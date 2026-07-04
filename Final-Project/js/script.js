cba17b803c48bd0bcac0021a5fb0f721cd9773a4
1muhamed-adel <muhamed.adel.fci@gmail.com>
2026-07-04 11:04:16 +0300

    fashion page

--- Final-Project/js/script.js
+++ Final-Project/js/script.js
@@ -1,2 +1,5 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#navbar-example2'
+})
